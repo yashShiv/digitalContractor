9f5ad79c57a5f41094e53b71ad086ce47e119c8c
abhisarshukla <abhisarshukla645@gmail.com>
2021-03-19 13:20:53 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-<<<<<<< HEAD
-import HomePage from './components/home/Home';
-
-function App() {
-  return (
-    <div className="App">
-    <HomePage></HomePage>
-=======
 import Login from './components/login/Login';
 import { BrowserRouter } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 import Register from './components/register/Register';
+import HomePage from './components/home/Home';
+import Footer from './components/footer/Footer';
+
 function App() {
   return (
     <div className="App">
@@ -20,11 +15,10 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-
+        <Route exact path="/" component={HomePage} />
       </Switch>
-
     </BrowserRouter>
->>>>>>> f57a39a3162b8a4670cec1d618c21e8dff94b7f9
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => {
+
+  return (
+    <div className="footer">
+      <p className="website">digitalcontrator.com</p>
+      <p className="copyright">&#169; 2020-21.</p>
+    </div>
+  )
+
+}
+
+export default Footer;
