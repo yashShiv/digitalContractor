ec187dd9e1155f49b9f39d935e7a146275c9afd0
abhisarshukla <abhisarshukla645@gmail.com>
2021-03-19 13:22:03 +0530

    Add HomePage component More details will be added later after discussion.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,12 +6,31 @@ const HomePage = () => {
   return (
     <div className = "home">
       <div className = "top">
-        <button class="top__button">Register</button>
-        <button class="top__button">Login</button>
+        <div className="top__buttons">
+          <button class="top__button">Register</button>
+          <button class="top__button">Login</button>
+        </div>
       </div>
       <main className = "info">
         <div className="info__section">
-          <div className="info__section__text"></div>
+          <div className="info__section__text">
+            <h1>Best Plumbing Services.</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et culpa in, quibusdam consequatur magni voluptate quidem similique suscipit voluptatem temporibus labore sint saepe doloremque. Tenetur dolorum laboriosam temporibus quasi tempora.
+          </div>
+          <div className="info__section__img"></div>
+        </div>
+        <div className="info__section">
+          <div className="info__section__img"></div>
+          <div className="info__section__text">
+            <h1>Best Plumbing Services.</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et culpa in, quibusdam consequatur magni voluptate quidem similique suscipit voluptatem temporibus labore sint saepe doloremque. Tenetur dolorum laboriosam temporibus quasi tempora.
+          </div>
+        </div>
+        <div className="info__section">
+          <div className="info__section__text">
+            <h1>Best Plumbing Services.</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et culpa in, quibusdam consequatur magni voluptate quidem similique suscipit voluptatem temporibus labore sint saepe doloremque. Tenetur dolorum laboriosam temporibus quasi tempora.
+          </div>
           <div className="info__section__img"></div>
         </div>
       </main>
