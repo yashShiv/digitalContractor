856b838fba26be4d89cee7b5b48c501e12a3e4ab
abhisarshukla <abhisarshukla645@gmail.com>
2021-03-19 20:42:58 +0530

    Start working on profile page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import Login from './components/login/Login';
+import "./App.css";
+import Login from "./components/login/Login";
 import { BrowserRouter } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
-import Navbar from './components/navbar/Navbar';
-import Register from './components/register/Register';
-import HomePage from './components/home/Home';
-import Footer from './components/footer/Footer';
+import Navbar from "./components/navbar/Navbar";
+import Register from "./components/register/Register";
+import HomePage from "./components/home/Home";
+import Footer from "./components/footer/Footer";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Navbar/>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/" component={HomePage} />
-      </Switch>
-    </BrowserRouter>
-    <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
