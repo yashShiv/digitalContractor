d16c7c2320bda9f7da38deba68720693f3d9eec6
abhisarshukla <abhisarshukla645@gmail.com>
2021-03-20 11:34:18 +0530

    Make better footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import './style.css';
 
 const Footer = () => {
 
   return (
     <div className="footer">
+      <div className="footer__section">
+        <NavLink className="no_style_link" to="/about" >
+          <span className="footer__item">Read about us</span>
+        </NavLink>
+        <NavLink className="no_style_link" to="/contact">
+          <span className="footer__item">Contact us</span>
+        </NavLink>
+        <NavLink className="no_style_link" to="/services">
+          <span className="footer__item">Our Services</span>
+        </NavLink>
+      </div>
       <p className="website">digitalcontrator.com</p>
       <p className="copyright">&#169; 2020-21.</p>
     </div>
