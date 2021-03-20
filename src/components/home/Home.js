f6551bc6159b14bde5bfb0d4e36d78a4f1c970d9
abhisarshukla <abhisarshukla645@gmail.com>
2021-03-20 11:50:00 +0530

    Fix NavLink style

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,10 +13,10 @@ const HomePage = () => {
         <h1>Select the kind of work you need to have, search for it in our services section, filter according to your budget and location.</h1>
         </div>
         <div className="top__buttons">
-        <NavLink exact to="/register">
+        <NavLink className="no_style_link" exact to="/register">
           <button className="top__button">Register</button>
         </NavLink>
-        <NavLink exact to="/login">
+        <NavLink className="no_style_link" exact to="/login">
           <button className="top__button">Login</button>
         </NavLink>
         </div>
